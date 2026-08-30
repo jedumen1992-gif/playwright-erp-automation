52ad2e75dee15acf3226935fb28079a5f023d696
jedumen1992-gif <jedu.men1992@gmail.com>
2026-08-30 11:53:55 -0300

    test: agregar espera en prueba de búsqueda

--- tests/primer_test.spec.js
+++ tests/primer_test.spec.js
@@ -1,7 +1,13 @@
 const { test } = require('@playwright/test');
 
-test('Abrir página', async ({ page }) => {
+test('Buscar elemento', async ({ page }) => {
 
     await page.goto('https://www.google.com');
+
+    const buscador = page.getByRole('combobox');
+
+    await buscador.fill('Playwright');
+
+    await page.waitForTimeout(3000);
 
 });
